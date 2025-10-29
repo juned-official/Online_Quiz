8da942301cb255b5ebfc370a785649a444c13750
Juned Maniyar <pseudo8433@gmail.com>
2025-10-29 12:10:38 +0530

    Auto-generated commit

---
+++ chunk-6YC6D2A4.js
@@ -0,0 +1,1 @@
+var e={production:!1,apiUrl:"https://261f417dc3eb.ngrok-free.app"};export{e as a};
--- chunk-S2Y6LMPX.js
+++
@@ -1,1 +0,0 @@
-var e={production:!1,apiUrl:"https://b2fa1fb76a04.ngrok-free.app"};export{e as a};
